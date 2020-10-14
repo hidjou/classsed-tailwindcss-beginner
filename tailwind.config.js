47211e6cef9596904d44225b52b306150cd77bd8
Ahmed Hadjou <ahmed@hadjou.com>
2020-10-14 09:44:23 +0100

    Done

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+    // defaultLineHeights: true,
+    // standardFontWeights: true
+  },
+  purge: [],
+  theme: {
+    fontFamily: {
+      body: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      margin: {
+        7: '1.75rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
